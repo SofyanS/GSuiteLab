dfbda0f5e4d08234fef7069cc12028a367c0e866
Sof <ssaputra@alumni.nd.edu>
2020-01-27 18:58:52 -0500

    first commit

---
+++ gsuite.sh
@@ -0,0 +1,15 @@
+# Move credentials file to correct folder
+mv ~/credentials.json .
+
+# Enable all APIs do this seperately in P1
+gcloud services enable drive.googleapis.com
+
+# Create storage bucket
+gsutil mb gs://bucket_$DEVSHELL_PROJECT_ID/
+
+# Upload credentials
+gsutil cp credentials.json gs://bucket_$DEVSHELL_PROJECT_ID/
+
+# Create two google docs before running this
+npm install
+node .
